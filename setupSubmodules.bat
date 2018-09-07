defe18bd9a7b409db6bf944b9cfb0d9db6e5f70e
Thomas Aunvik <thaun1@live.no>
2018-09-07 20:36:31 +0200

    Submodules

---
+++ setupSubmodules.bat
@@ -0,0 +1,4 @@
+git submodule add https://github.com/FFXIVAPP/sharlayan.git
+git submodule add https://github.com/FFXIVAPP/ffxivapp-props.git
+git submodule add https://github.com/Lachee/discord-rpc-csharp.git
+PAUSE
